8d80e9ae625f8520443dc772bc53fe5cb38d3566
lzizzhen <zizhen.li@zerotech.com>
2019-06-26 22:33:29 +0800

    AFNetworkReachabilityManager.m文件解读

--- AFNetworking/AFNetworkReachabilityManager.m
+++ AFNetworking/AFNetworkReachabilityManager.m
@@ -27,13 +27,27 @@
 #import <arpa/inet.h>
 #import <ifaddrs.h>
 #import <netdb.h>
-
+/**
+ * 网络环境发生改变的时候接受的通知
+ */
 NSString * const AFNetworkingReachabilityDidChangeNotification = @"com.alamofire.networking.reachability.change";
+/**
+ * 网络环境发生变化就会发送一个通知，同时携带一组状态数据，根据这个key来确定状态
+ */
 NSString * const AFNetworkingReachabilityNotificationStatusItem = @"AFNetworkingReachabilityNotificationStatusItem";
 
+/**
+ * 定义一个网络变化的回调
+ *
+ * @param status 网络状态参数，是一个枚举
+ */
 typedef void (^AFNetworkReachabilityStatusBlock)(AFNetworkReachabilityStatus status);
+
 typedef AFNetworkReachabilityManager * (^AFNetworkReachabilityStatusCallback)(AFNetworkReachabilityStatus status);
 
+/**
+ * 把枚举值转换为字符串
+ */
 NSString * AFStringFromNetworkReachabilityStatus(AFNetworkReachabilityStatus status) {
     switch (status) {
         case AFNetworkReachabilityStatusNotReachable:
@@ -47,12 +61,27 @@ NSString * AFStringFromNetworkReachabilityStatus(AFNetworkReachabilityStatus sta
             return NSLocalizedStringFromTable(@"Unknown", @"AFNetworking", nil);
     }
 }
+// *** NSLocalizedStringFromTable宏解读 --- 涉及到本地国际化的问题。所谓的国际化就是让你的app能够根据不同的语言显示相对应的语言。
+// *** 但这并不简单，没有经验的开发人员，一开始可能不会做这样的设置，如果日后需要国际话了，在做就很麻烦了。所以说在开中，但凡使用到字符串的地方都要考虑语言的不同。不同的语言下，一个意思的表达所使用的字符串长度是不一样的，这就影射出空间的宽度可能会不一样。
 
+
+/**
+ 2  *  根据SCNetworkReachabilityFlags这个网络标记来转换成我们在开发中经常使用的网络状态
+ 3      1.不能连接网络
+ 4      2.蜂窝连接
+ 5      3.WiFi连接
+ 6      4.未知连接
+ 7  */
 static AFNetworkReachabilityStatus AFNetworkReachabilityStatusForFlags(SCNetworkReachabilityFlags flags) {
+    // 是否能够到达
     BOOL isReachable = ((flags & kSCNetworkReachabilityFlagsReachable) != 0);
+    // 在联网之前需要建立连接
     BOOL needsConnection = ((flags & kSCNetworkReachabilityFlagsConnectionRequired) != 0);
+    // 是否可以自动连接
     BOOL canConnectionAutomatically = (((flags & kSCNetworkReachabilityFlagsConnectionOnDemand ) != 0) || ((flags & kSCNetworkReachabilityFlagsConnectionOnTraffic) != 0));
+    // 是否可以连接，在不需要用户手动设置的前提下
     BOOL canConnectWithoutUserInteraction = (canConnectionAutomatically && (flags & kSCNetworkReachabilityFlagsInterventionRequired) == 0);
+    // 是否可以联网的条件 1.能够到达 2.不需要建立连接或者不需要用户手动设置连接 就表示能够连接到网络
     BOOL isNetworkReachable = (isReachable && (!needsConnection || canConnectWithoutUserInteraction));
 
     AFNetworkReachabilityStatus status = AFNetworkReachabilityStatusUnknown;
@@ -70,8 +99,14 @@ static AFNetworkReachabilityStatus AFNetworkReachabilityStatusForFlags(SCNetwork
 
     return status;
 }
+// *** 这个方法根据SCNetworkReachabilityFlags这个标记转换成我们自定义的枚举类型。
+// *** 在这里不得不多说几句，很多框架中都会把一个类中的私有方法写成这样。为什么呢？ 我们在开发中经常会写成- (void)funcName; 这样的私有方法。
+// *** 我个人的意见是一个类中的私有方法写成static void funcName() 这样的c函数比较好。
+// *** 1. 在文件的最前方，比较容易查找
+// *** 2. 可以适当的使用内联函数，提高效率。
 
-/**
+
+/** 根据一个标识 来处理Block和通知。保证两者同一状态。
  * Queue a status change notification for the main thread.
  *
  * This is done to ensure that the notifications are received in the same order
@@ -114,7 +149,7 @@ static void AFNetworkReachabilityReleaseCallback(const void *info) {
 @end
 
 @implementation AFNetworkReachabilityManager
-
+// 单例
 + (instancetype)sharedManager {
     static AFNetworkReachabilityManager *_sharedManager = nil;
     static dispatch_once_t onceToken;
@@ -125,6 +160,9 @@ static void AFNetworkReachabilityReleaseCallback(const void *info) {
     return _sharedManager;
 }
 
+/**
+ * 通过一个domain地址来初始化。 首先新建SCNetworkReachabilityRef对象，然后调用initWithReachability: 方法。记得手动管理内存。
+ */
 + (instancetype)managerForDomain:(NSString *)domain {
     SCNetworkReachabilityRef reachability = SCNetworkReachabilityCreateWithName(kCFAllocatorDefault, [domain UTF8String]);
 
@@ -135,15 +173,21 @@ static void AFNetworkReachabilityReleaseCallback(const void *info) {
     return manager;
 }
 
+/**
+ * 通过一个socket地址来初始化。 首先新建 SCNetworkReachabilityRef 对象，然后调用initWithReachability: 方法。记得手动管理内存。
+ */
 + (instancetype)managerForAddress:(const void *)address {
     SCNetworkReachabilityRef reachability = SCNetworkReachabilityCreateWithAddress(kCFAllocatorDefault, (const struct sockaddr *)address);
     AFNetworkReachabilityManager *manager = [[self alloc] initWithReachability:reachability];
 
-    CFRelease(reachability);
+    CFRelease(reachability); // CFRetain()后要记得CFRelease().
     
     return manager;
 }
 
+/**
+ * 由于IPv6 是ios9和os_x 10.11后边推出的，所有要进行版本判断。
+ */
 + (instancetype)manager
 {
 #if (defined(__IPHONE_OS_VERSION_MIN_REQUIRED) && __IPHONE_OS_VERSION_MIN_REQUIRED >= 90000) || (defined(__MAC_OS_X_VERSION_MIN_REQUIRED) && __MAC_OS_X_VERSION_MIN_REQUIRED >= 101100)
@@ -159,6 +203,8 @@ static void AFNetworkReachabilityReleaseCallback(const void *info) {
 #endif
     return [self managerForAddress:&address];
 }
+// *** 1.方法的创建也是有顺序的，可以使用函数访问函数的思想。
+// *** 2. @if 这样的预编译指令能够替换掉代码中部分if else 。好处就是代码会不会被编译的区别。
 
 - (instancetype)initWithReachability:(SCNetworkReachabilityRef)reachability {
     self = [super init];
@@ -190,6 +236,10 @@ static void AFNetworkReachabilityReleaseCallback(const void *info) {
 
 #pragma mark -
 
+/**
+ * 这个是.h文件暴露出来的3个BOOL 属性的getter方法，注意，由于我们在@property中定义了getter方法，所以getter方法就要写成我们定义的那种。
+ * 从这3个方法中也能看出，函数嵌套的思想还是很重要，要想做到这一点，只能是多想才行。
+ */
 - (BOOL)isReachable {
     return [self isReachableViaWWAN] || [self isReachableViaWiFi];
 }
@@ -204,6 +254,9 @@ static void AFNetworkReachabilityReleaseCallback(const void *info) {
 
 #pragma mark -
 
+/**
+ * 设置监听网络监听
+ */
 - (void)startMonitoring {
     [self stopMonitoring];
 
@@ -223,10 +276,15 @@ static void AFNetworkReachabilityReleaseCallback(const void *info) {
         return strongSelf;
     };
 
+    // 设置网络监控分为下边几个步骤
+    // 1.我们先新建上下文
     SCNetworkReachabilityContext context = {0, (__bridge void *)callback, AFNetworkReachabilityRetainCallback, AFNetworkReachabilityReleaseCallback, NULL};
+    // 2.设置回调
     SCNetworkReachabilitySetCallback(self.networkReachability, AFNetworkReachabilityCallback, &context);
+    // 3.加入RunLoop池 (其中CFRunLoopGetMain()代表主RunLoop)
     SCNetworkReachabilityScheduleWithRunLoop(self.networkReachability, CFRunLoopGetMain(), kCFRunLoopCommonModes);
 
+    // 在异步线程 发送一次当前的网络状态。
     dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_BACKGROUND, 0),^{
         SCNetworkReachabilityFlags flags;
         if (SCNetworkReachabilityGetFlags(self.networkReachability, &flags)) {
@@ -234,7 +292,20 @@ static void AFNetworkReachabilityReleaseCallback(const void *info) {
         }
     });
 }
+// *** 解析
+/*
+ typedef struct {
+ CFIndex        version;
+ void *        __nullable info; // 相当于oc的id类型，void * 可以指向任何类型的参数
+ const void    * __nonnull (* __nullable retain)(const void *info); // 函数 目的是对info做retain操作
+ void        (* __nullable release)(const void *info); // 函数，目的是对info做release操作
+ CFStringRef    __nonnull (* __nullable copyDescription)(const void *info); // 函数，根据info获取Description字符串
+ } SCNetworkReachabilityContext;  在这里我们要携带的这个info就是下边的这个block
+ */
 
+/**
+ * 停止网络监控
+ */
 - (void)stopMonitoring {
     if (!self.networkReachability) {
         return;
@@ -256,7 +327,9 @@ static void AFNetworkReachabilityReleaseCallback(const void *info) {
 }
 
 #pragma mark - NSKeyValueObserving
-
+/**
+ * 注册键值依赖，这个可能大家平时用的比较少。可以了解一下
+ */
 + (NSSet *)keyPathsForValuesAffectingValueForKey:(NSString *)key {
     if ([key isEqualToString:@"reachable"] || [key isEqualToString:@"reachableViaWWAN"] || [key isEqualToString:@"reachableViaWiFi"]) {
         return [NSSet setWithObject:@"networkReachabilityStatus"];
